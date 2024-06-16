652fade1ad031787ab639f3103da92f0fd15024c
mynameisrobert99 <119703737+mynameisrobert99@users.noreply.github.com>
2024-06-16 16:16:06 -0500

    adding part 1-3 of js homework

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,6 +1,15 @@
 // Declare and initialize the variables for exercise 1 here:
+let engineIndicatorLight = 'red blinking';
+let spaceSuitOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
+
+// I predict it will say 'engine lights are off'
 
 if (engineIndicatorLight === "green") {
    console.log("engines have started");
@@ -9,3 +18,6 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
+
+
+
