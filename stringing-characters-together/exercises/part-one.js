4f1e02b788f2fa39827c9928a3dbf8264543aec7
mynameisrobert99 <119703737+mynameisrobert99@users.noreply.github.com>
2024-06-19 20:04:08 -0500

    string equasions

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -8,3 +8,8 @@ console.log(num.length);
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+if (String(num).includes('.')){
+    console.log(String(num).length-1);
+    } else {
+    console.log(String(num).length);
+    }
