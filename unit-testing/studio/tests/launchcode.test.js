18ed65114eebe8e3a31bfc315c07f024d4348260
mynameisrobert99 <119703737+mynameisrobert99@users.noreply.github.com>
2024-07-15 23:03:34 -0500

    unit testing

--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,17 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
-
-  // Write your unit tests here!
   
+  // Write your unit tests here!
+  test("organization should be 'nonprofit'", function() {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
 });
+
+test("executiveProducer should be 'jeff'", function() {
+  expect(launchcode.executiveDirector).toBe("Jeff");
+});  
+
+test("percentageCoolEmployees should be '100'", function() {
+  expect(launchcode.percentageCoolEmployees).toBe(100)
+})
