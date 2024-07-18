49d72fbbc8ebfbca2536b5d96e50d2b4e76d753e
mynameisrobert99 <119703737+mynameisrobert99@users.noreply.github.com>
2024-07-18 15:12:10 -0500

    exceptions

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,8 +1,11 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
+    try {
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
+  } catch (error) {
+    }
   }
 }
 
@@ -21,4 +24,26 @@ let studentLabs = [
   }
 ];
 
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
 gradeLabs(studentLabs);
+gradeLabs(studentLabs2);
